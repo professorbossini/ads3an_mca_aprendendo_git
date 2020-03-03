8b4d652d5c3a38cfe44ba17660686d1944cfb9b7
Rodrigo Bossini <professorbossini@gmail.com>
2020-03-02 22:33:55 -0300

    Segunda versão

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class EntregaGit {
     public static void main(String[] args) {
         System.out.println("Entregando tarefas com o Git na linha de comando...");
+        String nome = JOptionPane.showInputDialog("Qual seu nome?");
+        JOptionPane.showMessageDialog(null, "Oi, " + nome);
     }
 }
